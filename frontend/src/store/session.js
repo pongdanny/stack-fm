5d35ea2e165569c68561b197e4dac6a9470e845f
pongdanny <pongdaniel17@gmail.com>
2020-11-20 19:37:52 -0500

    logout functionality

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -49,6 +49,14 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+export const logout = () => async (dispatch) => {
+  const response = await fetch("/api/session", {
+    method: "DELETE",
+  });
+  dispatch(removeUser());
+  return response;
+};
+
 export const signup = (user) => async (dispatch) => {
   const { username, email, password } = user;
   const response = await fetch("/api/users", {
