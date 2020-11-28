21a34d1351ad93694debd14e2d8335f9e36036cd
pongdanny <pongdaniel17@gmail.com>
2020-11-28 16:28:34 -0500

    added build

--- frontend/src/components/Track/Track.js
+++ frontend/src/components/Track/Track.js
@@ -1,18 +1,36 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, Redirect } from "react-router-dom";
+import { songThunk } from "../store/charts";
 
-function Track({ playing }) {
+export default function Song() {
+  const songs = useSelector((state) => state.song);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(songThunk());
+  }, []);
+
   const sessionUser = useSelector((state) => state.session.user);
-
   if (!sessionUser) return <Redirect to="/login" />;
+
   return (
-    <div className="track-div">
-      {/* <div>{playing.songName}</div>
-      <div>{playing.artistName}</div>
-      <div>{playing.albumName}</div> */}
-    </div>
+    <ul className="">
+      {songs.map((song) => (
+        <div key={song.songName} className="">
+          <div className="">
+            <div className="">
+              <p>Song: {song.songName}</p>
+              <p>Artist: {song.artistName}</p>
+              <p>Album: {song.albumName}</p>
+            </div>
+          </div>
+          <div className=""></div>
+          <p></p>
+        </div>
+      ))}
+    </ul>
   );
 }
 
-export default Track;
+// export default Track;
