23522e8aa4b35384067e7d27304c704e31564bc9
pongdanny <pongdaniel17@gmail.com>
2020-11-28 12:41:06 -0500

    attempting audio component

--- frontend/src/components/Track/Track.js
+++ frontend/src/components/Track/Track.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
-import "./Track.css";
 
 function Track({ start }) {
   const sessionUser = useSelector((state) => state.session.user);
