01218a53d70e834f274338d6d7bbc62bb7e091fd
pongdanny <pongdaniel17@gmail.com>
2020-11-29 13:16:26 -0500

    html shows but not fetched data

--- frontend/src/components/ProfilePage/ProfilePage.js
+++ frontend/src/components/ProfilePage/ProfilePage.js
@@ -3,22 +3,13 @@ import "./ProfilePage.css";
 const ProfilePage = (props) => {
   return (
     <>
-      <div className="usertitle">
-        Hey {props.user.username} !<div className="userbody"></div>
-      </div>
+      <div className="usertitle">Welcome To Your Profile!</div>
       <div className="userdata">
-        <button onClick={props.openMenu}>
-          <i className="fas fa-user-circle" />
-        </button>
-        {props.showMenu && (
-          <ul className="profile-dropdown">
-            <li>{props.user.username}</li>
-            <li>{props.user.email}</li>
-            <li>
-              <button onClick={props.logout}>Log Out</button>
-            </li>
-          </ul>
-        )}
+        {/* <div>{props.user.username}</div> */}
+        {/* <div>{props.user.email}</div> */}
+        <div>
+          <button onClick={props.logout}>Log Out</button>
+        </div>
       </div>
     </>
   );
