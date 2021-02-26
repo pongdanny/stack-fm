e31497933fc7980cf7f1696183f0320b0c210d79
danny <62034468+pongdanny@users.noreply.github.com>
2021-02-26 09:55:14 -0500

    Update AboutPage.js

--- frontend/src/components/AboutPage/AboutPage.js
+++ frontend/src/components/AboutPage/AboutPage.js
@@ -6,8 +6,7 @@ const AboutPage = (props) => {
     <div>
       <div className="aboutstory">
         hey there! my name's danny and i'm currently a student at app academy,
-        learning to become a full stack engineer! im into hip hop and video
-        games but i hope to start an exciting new journey in software
+        learning to become a full stack engineer! I hope to start an exciting new journey in software
         development!
       </div>
     </div>
