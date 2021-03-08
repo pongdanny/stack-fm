13a58d90a67d5d300fe4c849c6d0c3e40e590d3f
danny <62034468+pongdanny@users.noreply.github.com>
2021-03-08 13:16:50 -0500

    cleaned out frontend code

--- frontend/src/components/songComp/Song.js
+++ frontend/src/components/songComp/Song.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// import { useDispatch } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { fetchSongs } from "../../store/charts";
 import "./Song.css";
@@ -13,8 +12,7 @@ export default function Song() {
     dispatch(fetchSongs());
   }, [dispatch]);
 
-  // const sessionUser = useSelector((state) => state.session.user);
-  // if (!sessionUser) return <Redirect to="/login" />;
+
   if (!songs) {
     return null;
   }
@@ -23,13 +21,13 @@ export default function Song() {
       <div>
         <div>
           <ul className="songlist">
-            {/* {songs.map((song) => ( */}
+
             <div>
               <li>Song {song.songName}</li>
               <li>Artist {song.artistName}</li>
               <li>Album {song.albumName}</li>
             </div>
-            {/* ))} */}
+
           </ul>
         </div>
       </div>
